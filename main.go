a44522db7b8109b4d47afcd8ebd2727f8b6de985
Edward Murakami <epmeddie@live.ca>
2019-03-02 14:49:53 -0500

    allow CORS

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -14,5 +15,9 @@ func main() {
 
 	ep.RegisterEndpoints()
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(originsOk, headersOk, methodsOk)(r)))
 }
